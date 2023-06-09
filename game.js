28d496d7e49c43c45f4100bd61a2cf3a63732366
Blue-Whale-Game <136090296+Blue-Whale-Game@users.noreply.github.com>
2023-06-09 19:07:34 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -1,1 +1,30 @@
-// JavaScript-код здесь
+const blueWhaleImage = new Image();
+blueWhaleImage.src = 'path/to/Blue_Whale_01.png';
+
+const obstacleImage = new Image();
+obstacleImage.src = 'path/to/Blue_Whale_obstacle.png';
+
+// Дождитесь загрузки изображений перед началом игры
+Promise.all([blueWhaleImage.onload, obstacleImage.onload]).then(startGame);
+
+function startGame() {
+  // Здесь можно начинать игру
+}
+function animate() {
+  // Очистите экран
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Отрисуйте спрайты
+  context.drawImage(blueWhaleImage, blueWhaleX, blueWhaleY);
+  context.drawImage(obstacleImage, obstacleX, obstacleY);
+
+  // Обновите позиции спрайтов
+  blueWhaleX += blueWhaleSpeedX;
+  obstacleX -= obstacleSpeedX;
+
+  // Вызовите функцию анимации снова
+  requestAnimationFrame(animate);
+}
+
+// Запустите анимацию
+animate();
