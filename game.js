ec5d00ffe9a9b00f9ce5cf9e30644aa0485df05f
Blue-Whale-Game <136090296+Blue-Whale-Game@users.noreply.github.com>
2023-06-10 19:57:41 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -1,29 +1,11 @@
 const canvas = document.getElementById('gameCanvas');
 const context = canvas.getContext('2d');
 
-let blueWhaleX = canvas.width / 2;
-let blueWhaleY = canvas.height / 2;
-
-let obstacleX = canvas.width;
-let obstacleY = canvas.height / 2;
-let obstacleWidth = 76;
-let obstacleHeight = 100;
-
-let meduzaX = canvas.width;
-let meduzaY = getRandomYPosition();
-let meduzaWidth = obstacleWidth;
-let meduzaHeight = obstacleHeight;
-
-let blueWhaleSpeedX = 1;
-let obstacleSpeedX = 3;
-
 const blueWhaleImage = new Image();
 blueWhaleImage.src = 'Blue_Whale_01.png';
-blueWhaleImage.onload = startGame;
 
 const obstacleImage = new Image();
 obstacleImage.src = 'Blue_Whale_obstacle.png';
-obstacleImage.onload = startGame;
 
 const meduzaImage = new Image();
 meduzaImage.src = 'Meduza_Blue_Whale.png';
@@ -31,49 +13,88 @@ meduzaImage.src = 'Meduza_Blue_Whale.png';
 const background = new Image();
 background.src = 'Blue_Whale_BG.png';
 
-let imagesLoaded = 0;
+let blueWhaleX = canvas.width / 2;
+let blueWhaleY = canvas.height / 2;
+let blueWhaleSpeedX = 1;
+
+let obstacleX = canvas.width;
+let obstacleY = canvas.height / 2;
+let obstacleWidth = 76;
+let obstacleHeight = 100;
+let obstacleSpeedX = 3;
+
+let meduzaX = canvas.width;
+let meduzaY = getRandomYPosition();
+let meduzaWidth = obstacleWidth;
+let meduzaHeight = obstacleHeight;
+
 let isJumping = false;
 let score = 0;
+let scoreElement = document.getElementById('score');
+let timerElement = document.getElementById('timer');
+
+let gameTimer;
+let gameTime = 0;
 
 function startGame() {
-  imagesLoaded++;
-  if (imagesLoaded === 4) {
-    animate();
-  }
+  context.drawImage(background, 0, 0, canvas.width, canvas.height);
+  animate();
+  animateObstacles();
+  startTimer();
 }
-
-document.addEventListener('keydown', handleKeyDown);
-
-function handleKeyDown(event) {
-  if (event.code === 'Space' && !isJumping) {
-    isJumping = true;
-    blueWhaleY -= 50;
-  }
-}
+// ...
 
 function animate() {
   context.clearRect(0, 0, canvas.width, canvas.height);
 
-  // Draw background
-  context.drawImage(background, 0, 0, canvas.width, canvas.height);
-
   blueWhaleX += blueWhaleSpeedX;
   obstacleX -= obstacleSpeedX;
-  meduzaX -= obstacleSpeedX;
 
   if (obstacleX + obstacleWidth < 0) {
     obstacleX = canvas.width;
-  }
-
-  if (meduzaX + meduzaWidth < 0) {
-    meduzaX = canvas.width;
-    meduzaY = getRandomYPosition();
   }
 
   if (isJumping) {
     blueWhaleY += 5;
     if (blueWhaleY >= canvas.height / 2) {
       isJumping = false;
+    }
+  }
+
+  context.drawImage(blueWhaleImage, blueWhaleX, blueWhaleY);
+  context.drawImage(obstacleImage, obstacleX, obstacleY, obstacleWidth, obstacleHeight);
+
+  if (blueWhaleX > obstacleX + obstacleWidth) {
+    score += 10;
+    updateScore();
+  }
+
+  // ...
+
+  requestAnimationFrame(animate);
+}
+
+// ...
+
+function animate() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+  blueWhaleX += blueWhaleSpeedX;
+
+  if (blueWhaleX + blueWhaleImage.width > canvas.width) {
+    blueWhaleX = canvas.width - blueWhaleImage.width;
+  }
+
+  if (isJumping) {
+    blueWhaleY -= 5;
+    if (blueWhaleY <= 0) {
+      isJumping = false;
+    }
+  } else {
+    blueWhaleY += 5;
+    if (blueWhaleY + blueWhaleImage.height > canvas.height) {
+      endGame();
     }
   }
 
@@ -97,16 +118,29 @@ function animate() {
     meduzaX = -meduzaWidth;
   }
 
-  if (
-    (blueWhaleX < obstacleX + obstacleWidth &&
-      blueWhaleX + blueWhaleImage.width > obstacleX &&
-      blueWhaleY < obstacleY + obstacleHeight &&
-      blueWhaleY + blueWhaleImage.height > obstacleY) ||
-    blueWhaleY + blueWhaleImage.height >= canvas.height
-  ) {
-    endGame();
-  } else {
-    requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
+}
+
+function animateObstacles() {
+  obstacleX -= obstacleSpeedX;
+  meduzaX -= obstacleSpeedX;
+
+  if (obstacleX + obstacleWidth < 0) {
+    obstacleX = canvas.width;
+  }
+
+  if (meduzaX + meduzaWidth < 0) {
+    meduzaX = canvas.width;
+    meduzaY = getRandomYPosition();
+  }
+
+  requestAnimationFrame(animateObstacles);
+}
+
+function handleKeyDown(event) {
+  if (event.code === 'Space' && !isJumping) {
+    isJumping = true;
+    blueWhaleY -= 50;
   }
 }
 
@@ -115,18 +149,30 @@ function getRandomYPosition() {
 }
 
 function updateScore() {
-  document.getElementById('score').textContent = `Score: ${score}`;
+  scoreElement.textContent = `Score: ${score}`;
+}
+
+function startTimer() {
+  gameTimer = setInterval(updateTimer, 1000);
+}
+
+function updateTimer() {
+  gameTime++;
+  timerElement.textContent = `Time: ${gameTime}s`;
 }
 
 function endGame() {
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  context.fillStyle = 'black';
-  context.fillRect(0, 0, canvas.width, canvas.height);
-  context.font = '30px Arial';
-  context.fillStyle = 'white';
-  context.textAlign = 'center';
-  context.fillText('GAME OVER', canvas.width / 2, canvas.height / 2);
+  clearInterval(gameTimer);
+  document.removeEventListener('keydown', handleKeyDown);
+  // Дополнительные действия по завершению игры, например, вывод результата
 }
 
-updateScore();
-animate();
+blueWhaleImage.onload = function () {
+  obstacleImage.onload = function () {
+    meduzaImage.onload = function () {
+      startGame();
+    }
+  }
+};
+
+document.addEventListener('keydown', handleKeyDown);
